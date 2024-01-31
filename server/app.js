98a3b3dc2eb9875d9f5e52848bb7fae8172bbfd9
Nirmal <nirmalraj1025@gmail.com>
2024-01-31 16:57:56 +0530

    update

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
 import cors from "cors";
 import { config } from "dotenv";
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
+// import path from "path";
+// import { fileURLToPath } from "url";
 import paymentRouter from "./routes/paymentRoute.js";
 
 export const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 // Config
 if (process.env.NODE_ENV !== "PRODUCTION") {
@@ -21,8 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", paymentRouter);
 
-app.use(express.static(path.join(__dirname, "../client/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve("../client/build/index.html"));
-});
+// app.use(express.static(path.join(__dirname, "../client/build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve("../client/build/index.html"));
+// });
